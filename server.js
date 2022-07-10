4a68009abd2f8f28195138acb23d5cf66dfca48b
jag <jagrecco@gmail.com>
2022-07-10 20:32:33 -0300

    Tercer commit servidor

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const prueba = new Contenedor("productos.txt")
 prueba.leerObjetos()
 
 app.get("/", (req, res) => {
-  res.send('<h1>Bienvenidos al servidor mi primer express</h1>'
+  res.send('<h1>¡Bienvenidos al servidor mi primer express!</h1>'
   );
   
 });
